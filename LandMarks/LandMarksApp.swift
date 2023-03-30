d73b1a356ebdd10ebcf55532482f86139256bb29
Nour Ajaj <noajaj@cps.edu>
2023-03-30 14:04:19 -0500

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Ajaj, Nour O on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
